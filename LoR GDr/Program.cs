52f505e2876ded081b6a816978c46b2fa2f2e329
daniele.pieri <daniele.pieri.stud@ispascalcomandini.it>
2023-12-16 12:54:29 +0100

    16-12-2023_classe

---
+++ LoR GDr/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace LoR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CampiDelPellennor campoDiBattaglia = new CampiDelPellennor();
+            campoDiBattaglia.MenuCreazione();
+        }
+    }
+
+    class CampiDelPellennor
+    {
+        public void MenuCreazione()
+        {
+            // Your implementation for MenuCreazione goes here
+        }
+    }
+
+    interface ICombattente
+    {
+        int Vita { get; set; }
+        int Forza { get; set; }
+        int Esperienza { get; set; }
+        int Danno { get; set; }
+
+        void CalcolaVita()
+        {}
+        void CalcolaForza()
+        {}
+        void CalcolaEsperienza()
+        {}
+        void CalcolaDanno()
+        {}
+    }
+
+    interface IBattaglione
+    {
+        int VitaTotale { get; set; }
+        int ForzaTotale { get; set; }
+        int EsperienzaTotale { get; set; }
+        int DannoTotale { get; set; }
+
+        void   CalcolaVita()
+        {
+        }
+        void CalcolaForza()
+        {
+        }
+        void CalcolaEsperienza()
+        {
+        }
+        void CalcolaDanno()
+        {
+        }
+    }
+
+    }
